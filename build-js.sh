7d5070c057636b9abbac2f9391f7ce819bf44fc3
Andrew Sun <me@andrewsun.com>
2016-06-19 20:35:19 -0400

    Move mozjpeg to deps folder

--- build-js.sh
+++ build-js.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cd mozjpeg
+cd deps/mozjpeg
 emconfigure ./configure --without-simd --without-turbojpeg CFLAGS="-O3"
 emmake make
-cp .libs/cjpeg ../cjpeg.bc
-cd ..
-emcc -O3 --closure 1 --pre-js src/pre.js -s "EXPORTED_RUNTIME_METHODS=[]" -s ELIMINATE_DUPLICATE_FUNCTIONS=1 --memory-init-file 0 cjpeg.bc -o src/cjpeg.js -L'./mozjpeg/.libs' -ljpeg
+cp .libs/cjpeg ../../cjpeg.bc
+cd ../..
+emcc -O3 --closure 1 --pre-js src/pre.js -s "EXPORTED_RUNTIME_METHODS=[]" -s ELIMINATE_DUPLICATE_FUNCTIONS=1 --memory-init-file 0 cjpeg.bc -o src/cjpeg.js -L'./deps/mozjpeg/.libs' -ljpeg
