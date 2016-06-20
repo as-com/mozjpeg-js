a533face458c9eae6b0c4861cd558962c3243576
Andrew Sun <me@andrewsun.com>
2016-06-19 20:14:34 -0400

    Optimizations

--- src/pre.js
+++ src/pre.js
@@ -1,84 +1,76 @@
-// UMD
-(function(root, factory) {
-	if (typeof define === 'function' && define["amd"]) {
-		// AMD. Register as an anonymous module.
-		define([], factory);
-	} else if (typeof module === 'object' && module["exports"]) {
-		// Node. Does not work with strict CommonJS, but
-		// only CommonJS-like environments that support module.exports,
-		// like Node.
-		module["exports"] = factory();
-	} else {
-		// Browser globals (root is window)
-		root["returnExports"] = factory();
+function toUint8Array(buffer) {
+	var ab = new ArrayBuffer(buffer.length);
+	var view = new Uint8Array(ab);
+	for (var i = 0; i < buffer.length; ++i) {
+		view[i] = buffer[i];
 	}
-}(this, function() {
-	var x = {};
+	return view;
+}
 
-	function toUint8Array(buffer) {
-		var ab = new ArrayBuffer(buffer.length);
-		var view = new Uint8Array(ab);
-		for (var i = 0; i < buffer.length; ++i) {
-			view[i] = buffer[i];
-		}
-		return view;
+function toBuffer(ab) {
+	var buffer = new Buffer(ab.byteLength);
+	var view = new Uint8Array(ab);
+	for (var i = 0; i < buffer.length; ++i) {
+		buffer[i] = view[i];
 	}
+	return buffer;
+}
 
-	function toBuffer(ab) {
-		var buffer = new Buffer(ab.byteLength);
-		var view = new Uint8Array(ab);
-		for (var i = 0; i < buffer.length; ++i) {
-			buffer[i] = view[i];
-		}
-		return buffer;
-	}
+var stdout = "";
+var stderr = "";
 
-	x["encode"] = function(file, options) {
-		// file: Buffer containing file in PPM, PGM, BMP, or Targa format.
-		// options: hash containing options to pass to cjpeg
-		// returns: file contents?
+module.exports = function(file, options) {
+	// file: Buffer containing file in PPM, PGM, BMP, or Targa format.
+	// options: hash containing options to pass to cjpeg
+	// returns: file contents?
 
-		var stdout = "";
-		var stderr = "";
-		var args = ["-outfile", "/output.jpg"];
+	stdout = "";
+	stderr = "";
 
-		for (var key in options) {
-			if (!options.hasOwnProperty(key)) continue;
+	var args = ["-outfile", "/output.jpg"];
 
-			if (options[key]) {
-				args.push("-" + key);
-				if (typeof options[key] !== "boolean") {
-					// option has a value
-					args.push(String(options[key]));
-				}
+	for (var key in options) {
+		if (!options.hasOwnProperty(key)) continue;
+
+		if (options[key]) {
+			args.push("-" + key);
+			if (typeof options[key] !== "boolean") {
+				// option has a value
+				args.push(String(options[key]));
 			}
 		}
+	}
 
-		args.push("/input");
+	args.push("/input");
 
-		var Module = {
-			"print": function(text) {
-				stdout += text;
-			},
-			"printErr": function(text) {
-				stderr += text;
-			},
-			"preRun": [function() {
-				FS.writeFile("/input", toUint8Array(file), {
-					encoding: "binary"
-				});
-			}],
-			"arguments": args,
-			// "postRun": [ function () {
-			// 	var file;
-			// 	try { 
-			// 	file = FS.readFile("/output.jpg");
-			// 	} catch (e) {
-			// 	cb(e, null, stdout, stderr);
-			// 	return;
-			// 	}
-			// 	//console.log(file);
-			// 	cb(null, toBuffer(file.buffer));
-			// }],
-			"ENVIRONMENT": "SHELL" // maximum compatibility?
-		};
+	FS.writeFile("/input", toUint8Array(file), {
+		encoding: "binary"
+	});
+
+	Module["callMain"](args);
+
+	var file;
+	try { 
+		file = FS.readFile("/output.jpg");
+	} catch (e) {
+		return new Error("No output: " + stderr);
+	}
+	//console.log(file);
+	return {
+		"data": toBuffer(file.buffer),
+		"stderr": stderr
+	};
+}
+
+var Module = {
+	"noInitialRun" : true,
+	"noExitRuntime" : true,
+	"print": function(text) {
+		stdout += text;
+	},
+	"printErr": function(text) {
+		stderr += text;
+	},
+	"ENVIRONMENT": "SHELL"
+}
+
