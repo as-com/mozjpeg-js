ed93c48b9898965fb6e5a36ad6bf7c5fdce35a53
Andrew Sun <me@andrewsun.com>
2018-04-02 22:42:14 -0400

    Big update!

--- build-js.sh
+++ build-js.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd deps/mozjpeg
+autoreconf -fiv
 emconfigure ./configure --without-simd --without-turbojpeg CFLAGS="-O3"
-emmake make
+emmake make clean
+emmake make -j4
 cp .libs/cjpeg ../../cjpeg.bc
 cd ../..
 emcc -O3 --closure 1 --pre-js pre.js --post-js post.js -s "EXPORTED_RUNTIME_METHODS=[]" -s ELIMINATE_DUPLICATE_FUNCTIONS=1 -s ALLOW_MEMORY_GROWTH=1 --memory-init-file 0 --js-transform "node output-cleaner.js" cjpeg.bc -o lib/cjpeg.js -L'./deps/mozjpeg/.libs' -ljpeg
